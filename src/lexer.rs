c6eec969b82cf6754cf996d4513d1a21a91dfc31
Longor1996 <lalongok@gmail.com>
2021-09-18 18:29:50 +0200

    Move lexer-tests into submodule.

--- src/lexer.rs
+++ src/lexer.rs
@@ -232,39 +232,4 @@ pub mod literal;
 pub use literal::*;
 
 #[cfg(test)]
-mod tests {
-    #![allow(unused_imports)]
-    use super::tokenize;
-    
-    #[test]
-    fn lex_delimiters() {
-        tokenize("( () )").count();
-        tokenize("[ [] ]").count();
-        tokenize("{ {} }").count();
-        tokenize("< <> >").count();
-    }
-    
-    #[test]
-    fn lex_operators() {
-        tokenize("+ - * / ~ # , . : ; _ = ? ! $ % & | ^").count();
-    }
-    
-    #[test]
-    fn lex_strings() {
-        tokenize("abc-123 null test true false NaN infinity").count();
-        tokenize("\"\" \"Hello, World!\" '' 'Hello, World!'").count();
-    }
-    
-    #[test]
-    fn lex_numbers() {
-        tokenize("0 1 2 3 4 5 6 7 8 9 12345 +123 -123").count();
-        tokenize("0b101010 0o10 0d10 0x1F 0.1 0.001 0. 1e3 -0xFF").count();
-        tokenize("0.1 0.001 0. 1e3 10e-3 -0.5").count();
-    }
-    
-    #[test]
-    #[ignore]
-    fn lex_example() {
-        tokenize("blocks (b box 0 0 0 15 15 15) set air").inspect(|t| println!("{:?}",t)).count();
-    }
-}
+mod tests;
